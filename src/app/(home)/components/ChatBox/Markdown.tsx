ccf0a48167eca2da3b90cc2ccc279a29cd9ef170
Kiwon Kim <kiwon@elicer.com>
2024-12-09 01:26:39 +0900

    feat: code의 inner text 복사 기능 추가

--- src/app/(home)/components/ChatBox/Markdown.tsx
+++ src/app/(home)/components/ChatBox/Markdown.tsx
@@ -3,16 +3,70 @@ import rehypeHighlight from 'rehype-highlight';
 import styles from './Markdown.module.scss';
 
 import 'highlight.js/styles/github.css'; // 코드 하이라이팅 스타일 (원하는 테마 선택 가능)
+import CopyIcon from './CopyIcon';
 
 interface MarkdownProps {
   content: string;
 }
 
 const Markdown = ({ content }: MarkdownProps) => {
+  const generateId = () => {
+    // 36진수는 숫자(0-9)와 영어 알파벳 소문자(a-z)를 사용하여 표현
+    return `code-${Math.random().toString(36).slice(2, 11)}`; // 유니크한 ID 생성
+  };
+
+  const handleCopy = (codeId: string) => {
+    const codeElement = document.getElementById(codeId);
+    if (codeElement) {
+      const codeText = codeElement.innerText;
+      navigator.clipboard
+        .writeText(codeText)
+        .then(() => {
+          alert('코드가 복사되었습니다!');
+        })
+        .catch((error) => {
+          console.error('코드 복사 실패:', error);
+        });
+    }
+  };
+
   return (
     <ReactMarkdown
       className={styles.markdown}
       rehypePlugins={[rehypeHighlight]} // 코드 하이라이팅 플러그인
+      components={{
+        code({ node, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || '');
+          const codeId = generateId();
+
+          // Expect Code
+          if (match) {
+            return (
+              <div>
+                <button
+                  className="ml-auto mb-2 rounded-md border border-gray-300 text-gray-400 text-xs px-2 py-1 flex items-center gap-1 font-bold"
+                  onClick={() => handleCopy(codeId)}
+                >
+                  <CopyIcon />
+                  코드 복사
+                </button>
+                <pre>
+                  <code id={codeId} className={className} {...props}>
+                    {children}
+                  </code>
+                </pre>
+              </div>
+            );
+          }
+
+          // Inline
+          return (
+            <code className={className} {...props}>
+              {children}
+            </code>
+          );
+        },
+      }}
     >
       {content}
     </ReactMarkdown>
