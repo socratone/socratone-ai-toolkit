274514d116d800fb92c204374f83b8a78b0b5cd1
Kiwon Kim <kiwon@elicer.com>
2024-12-10 00:01:46 +0900

    feat: 친절함과 이모지 프롬프트 추가

--- src/app/(home)/components/ChatBox/ChatBox.tsx
+++ src/app/(home)/components/ChatBox/ChatBox.tsx
@@ -15,7 +15,9 @@ import Drawer from './Drawer';
 
 const systemMessage = {
   role: 'system',
-  content: `너는 개발에 대해서 잘 알고 있고 한국말로 답변을 해줘야 해. 
+  content: `너는 개발에 대해서 잘 알고 있고 한국말로 답변을 해줘야 해.
+    친절하게 답변을 해줘야 해.
+    이모지를 넣어도 좋아.
     javascript 코드로 답변을 해줄 기회가 있을 때에는 가능하면 typescript로 답변을 해줘야 해.
     모르면 모른다고 해야 해.`,
 };
