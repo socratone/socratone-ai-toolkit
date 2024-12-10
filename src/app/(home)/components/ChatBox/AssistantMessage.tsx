a30286437741fa7663fbb60b1ed2afb070f22134
Kiwon Kim <kiwon@elicer.com>
2024-12-10 22:28:13 +0900

    feat: ai 아바타 추가

--- src/app/(home)/components/ChatBox/AssistantMessage.tsx
+++ src/app/(home)/components/ChatBox/AssistantMessage.tsx
@@ -1,5 +1,7 @@
 import classNames from 'classnames';
 import Markdown from './Markdown';
+import Image from 'next/image';
+import aiImage from './images/ai-512x512.png';
 
 interface AssistantMessageProps {
   content: string;
@@ -8,8 +10,21 @@ interface AssistantMessageProps {
 
 const AssistantMessage = ({ content, className }: AssistantMessageProps) => {
   return (
-    <div className={classNames('p-3 rounded-lg border', className)}>
-      <Markdown content={content} />
+    <div className={classNames('flex gap-3', className)}>
+      <div className="flex-shrink-0">
+        <div className="size-12 rounded-full border border-gray-200 flex justify-center items-center">
+          <Image
+            className="block"
+            src={aiImage}
+            alt="AI"
+            width={24}
+            height={24}
+          />
+        </div>
+      </div>
+      <div className="p-3 rounded-lg border">
+        <Markdown content={content} />
+      </div>
     </div>
   );
 };
