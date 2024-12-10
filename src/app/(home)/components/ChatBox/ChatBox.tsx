eed5c6fd982a30bd608036e6083d44281d9fe377
Kiwon Kim <kiwon@elicer.com>
2024-12-10 23:35:06 +0900

    refactor: constants로 이동

--- src/app/(home)/components/ChatBox/ChatBox.tsx
+++ src/app/(home)/components/ChatBox/ChatBox.tsx
@@ -16,30 +16,7 @@ import Link from 'next/link';
 import CloseIcon from './icons/CloseIcon';
 import ScrollButton from './ScrollButton';
 import { debounce } from 'es-toolkit';
-
-const systemMessage = {
-  role: 'system',
-  content: `너는 개발에 대해서 잘 알고 있고 한국말로 답변을 해줘야 해.
-    친절하게 답변을 해줘야 해.
-    이모지를 넣어도 좋아.
-    javascript 코드로 답변을 해줄 기회가 있을 때에는 가능하면 typescript로 답변을 해줘야 해.
-    모르면 모른다고 해야 해.`,
-};
-
-const modelOptions: { value: OpenAiModel; label: string }[] = [
-  {
-    label: 'gpt-4o',
-    value: 'gpt-4o',
-  },
-  {
-    label: 'gpt-4o-mini',
-    value: 'gpt-4o-mini',
-  },
-  {
-    label: 'gpt-4',
-    value: 'gpt-4',
-  },
-];
+import { modelOptions, systemMessage } from './constants';
 
 const ChatBox = () => {
   const { register, handleSubmit, reset } = useForm<{ userMessage: string }>({
