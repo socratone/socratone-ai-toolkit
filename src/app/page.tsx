ce0416360bdc91125720202352a6e927e0320ab1
Kiwon Kim <gim2origin@gmail.com>
2025-02-18 00:55:58 +0900

    feat: 홈페이지 폰트 사이즈 개선

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import Link from 'next/link';
 const Home = () => {
   return (
     <div className="h-screen flex justify-center items-center">
-      <div className="flex flex-col space-y-4">
-        <Link className="text-center" href="/chat">
+      <div className="flex flex-col text-lg">
+        <Link className="text-center py-1 px-2 font-semibold" href="/chat">
           Chat
         </Link>
-        <Link className="text-center" href="/pdf">
+        <Link className="text-center py-1 px-2 font-semibold" href="/pdf">
           Pdf
         </Link>
       </div>
