6d9edc07cc269ea9a55ab1f642ba98773f12575c
Kiwon Kim <gim2origin@gmail.com>
2025-02-18 02:22:43 +0900

    style: format python codes

--- server/routes/pdf_routes.py
+++ server/routes/pdf_routes.py
@@ -1,20 +1,21 @@
 from flask import Blueprint, request, jsonify
 import PyPDF2
 
-pdf_blueprint = Blueprint('pdf', __name__)
+pdf_blueprint = Blueprint("pdf", __name__)
 
-@pdf_blueprint.route('/text-from-pdf', methods=['POST'])
+
+@pdf_blueprint.route("/text-from-pdf", methods=["POST"])
 def text_from_pdf():
     try:
         # 파일이 요청에 포함되어 있는지 확인
-        if 'file' not in request.files:
-            return jsonify({'error': 'No file part'}), 400
+        if "file" not in request.files:
+            return jsonify({"error": "No file part"}), 400
 
-        file = request.files['file']
+        file = request.files["file"]
 
         # 파일이 비어있는지 확인
-        if file.filename == '':
-            return jsonify({'error': 'No selected file'}), 400
+        if file.filename == "":
+            return jsonify({"error": "No selected file"}), 400
 
         # PDF 파일에서 텍스트 추출
         pdf_reader = PyPDF2.PdfReader(file)
@@ -22,7 +23,7 @@ def text_from_pdf():
         for page in pdf_reader.pages:
             text.append(page.extract_text())
 
-        return jsonify({'text': '\n'.join(text)})
+        return jsonify({"text": "\n".join(text)})
 
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({"error": str(e)}), 500
--- server/routes/youtube_routes.py
+++ server/routes/youtube_routes.py
@@ -3,21 +3,20 @@ from pytubefix import YouTube
 
 youtube_blueprint = Blueprint('youtube', __name__)
 
+
 @youtube_blueprint.route('/youtube-to-text', methods=['POST'])
 def youtube_to_text():
     data = request.json
     url = data.get('url')
-    
+
     if not url:
         return jsonify({'error': 'No URL provided'}), 400
-    
+
     try:
         yt = YouTube(url)
-        stream = yt.streams.filter(progressive=True, file_extension='mp4').first()
+        stream = yt.streams.filter(
+            progressive=True, file_extension='mp4').first()
         stream.download(output_path='downloads/')  # 다운로드할 경로 설정
         return jsonify({'message': 'Download started!'}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-
-
- 
