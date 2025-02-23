7eaefdca2723ac6475efdbb4328751ef1aa6c065
Kiwon Kim <gim2origin@gmail.com>
2025-02-24 00:37:27 +0900

    refactor: fill color를 tailwind class로 변경

--- src/components/EllipsisLoader.tsx
+++ src/components/EllipsisLoader.tsx
@@ -1,10 +1,7 @@
-/** bg-blue-200 */
-const BLUE_COLOR = '#bfdbfe';
-
 const EllipsisLoader = () => {
   return (
     <svg width="60" height="30" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="10" cy="15" r="5" fill={BLUE_COLOR}>
+      <circle cx="10" cy="15" r="5" className="fill-blue-200">
         <animate
           attributeName="opacity"
           values="1;0;1"
@@ -13,7 +10,7 @@ const EllipsisLoader = () => {
           begin="0s"
         />
       </circle>
-      <circle cx="30" cy="15" r="5" fill={BLUE_COLOR}>
+      <circle cx="30" cy="15" r="5" className="fill-blue-200">
         <animate
           attributeName="opacity"
           values="1;0;1"
@@ -22,7 +19,7 @@ const EllipsisLoader = () => {
           begin="0.2s"
         />
       </circle>
-      <circle cx="50" cy="15" r="5" fill={BLUE_COLOR}>
+      <circle cx="50" cy="15" r="5" className="fill-blue-200">
         <animate
           attributeName="opacity"
           values="1;0;1"
