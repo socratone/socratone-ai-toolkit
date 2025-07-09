50de2921ca7a094f64236dddf1ee256dd7cc6740
Kiwon Kim <gim2origin@gmail.com>
2025-07-09 22:16:16 +0900

    feat: 개발자 옵션 선택시 프론트엔드에 특정하지 않도록 메시지 수정

--- src/app/chat/components/ChatBox/constants.ts
+++ src/app/chat/components/ChatBox/constants.ts
@@ -4,14 +4,12 @@ export const devSystemMessage = {
   role: 'system',
   content: `너는 경험이 많은 시니어 개발자야. 다음 규칙을 따라야해.
 - 한국말을 사용한다.
-- 자바스크립트 대신 타입스크립트를 쓴다.
 - 코드 생성시 주석을 같이 표시한다.`,
 };
 
 export const systemMessage = {
   role: 'system',
-  content: `너는 한국말로 답변을 해줘야 해.
-      모르면 모른다고 해야 해.`,
+  content: `너는 한국말로 답변을 해줘야 해. 모르면 모른다고 해야 해.`,
 };
 
 export const deepseekSystemMessage = {
